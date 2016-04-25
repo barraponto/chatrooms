435e64d146b2ecf287489691b8c215192e51a4cc
Capi Etheriel <barraponto@gmail.com>
2016-04-25 16:10:07 -0300

    listen for feedback on user login

--- public/javascripts/components/app/app.controller.js
+++ public/javascripts/components/app/app.controller.js
@@ -4,4 +4,6 @@ angular.module('chatrooms').controller(
     $scope.$on('login:request', function(event, data){
       socket.emit('user:login', {username: data.username});
     });
+    socket.on('user:login:success', function(data){ console.log(data); });
+    socket.on('user:login:error', function(data){ console.log(data); });
 }]);
